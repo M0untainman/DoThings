c5a25788eb825f4c2145c91629e8d44ddba0ebcc
samuel <samuel.chowles@gmail.com>
2021-12-17 16:40:45 +0200

    creates task containers used to display inputed tasks

--- src/add.js
+++ src/add.js
@@ -1,3 +1,14 @@
 export default function addTask(text) {
     console.log(text);
+    const taskContainer = document.getElementById('taskContainer');
+    const taskItem = document.createElement('div');
+    taskItem.classList.add('taskItem');
+    taskContainer.appendChild(taskItem);
+    const taskText = document.createElement('p')
+    taskText.innerHTML = text;
+    taskItem.appendChild(taskText);
+    
+
+
+
 }
