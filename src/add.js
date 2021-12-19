76ceb9bf03f402b56695146105992496e39e7893
samuel <samuel.chowles@gmail.com>
2021-12-19 13:01:16 +0200

    can remove a task from task list

--- src/add.js
+++ src/add.js
@@ -18,10 +18,12 @@ const taskFactory = (taskTitle, taskDescription) =>{
 
 // function to display task info to the DOM
 const displayTasks = (taskArray) => {
-    taskArray.forEach((task) => {
-        const taskContainer = document.getElementById('taskContainer');
+    const taskContainer = document.getElementById('taskContainer');
+    taskContainer.innerHTML = '';
+    taskArray.forEach((task, index) => {
         const taskItem = document.createElement('div');
         taskItem.classList.add('taskItem');
+        taskItem.id = `${index}`
         taskContainer.appendChild(taskItem);
         const taskTitle = document.createElement('p')
         taskTitle.innerHTML = task.taskTitle; // title goes here
@@ -29,7 +31,22 @@ const displayTasks = (taskArray) => {
         const taskDescription = document.createElement('p')
         taskDescription.innerHTML = task.taskDescription; // description goes here
         taskItem.appendChild(taskDescription); 
+        const done = document.createElement('div')
+        done.classList.add('btn_done');
+        done.innerHTML = 'Done'
+        done.addEventListener('click', (e) => jobDone(e));
+        taskItem.appendChild(done);
     });
+
+const jobDone = (e) => {
+    //function to remove a book from library
+    console.log('jobdone started')
+    let taskList = JSON.parse(localStorage.getItem('taskList'))
+    let taskToRemove = e.srcElement.parentElement.id;
+    taskList.splice(taskToRemove, 1);
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+    displayTasks(taskList);
+}
     
 }
     
