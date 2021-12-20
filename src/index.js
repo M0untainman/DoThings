ff8fa2350908e6fd27cd6c8165fe1a7a5fc91f18
samuel <samuel.chowles@gmail.com>
2021-12-20 11:54:33 +0200

    add task popup form

--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ import displayTasks from './display.js';
 //dom declartations
 const btn_add = document.getElementById('btn_add');
 const btnClose = document.getElementById('btnClose')
+const btnSubmit = document.getElementById('btnSubmit')
+const title = document.getElementById('title');
+const description = document.getElementById('description')
 
 // event listeners
 btn_add.addEventListener('click', () => {
     openForm();
 })
 btnClose.addEventListener('click', () =>{
+    closeForm();
+})
+btnSubmit.addEventListener('click', () => {
+    addTask(title.value, description.value)
+    title.value = '';
+    description.value = '';
     closeForm();
 })
 
